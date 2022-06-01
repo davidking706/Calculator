96bc1fa0558373fb16b2db735edc6329a438ad34
davidking706 <davidking715@icloud.com>
2022-06-01 02:03:22 +0000

    grabbing numbers

--- calculator.js
+++ calculator.js
@@ -10,11 +10,14 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-    console.log(req.body);
+    var num1 = Number(req.body.n1);
+    var num2 = Number(req.body.n2);
 
-    res.send("Thanks for posting that!")
+    var result = num1 + num2;
+
+    res.send("The result of the calculation is: " + result);
 });
 
 app.listen(3000, function () {    
-    console.log("Server started on port 3000");
+    console.log("Server is running on port 3000");
 }); 
