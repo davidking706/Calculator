86316862f950d68cf5f507b64edf17b9f370f200
davidking706 <davidking715@icloud.com>
2022-06-01 01:16:50 +0000

    created js named path.js

--- calculator.js
+++ calculator.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send("<h1>Hello World! Calculator</h1>");
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function () {
+app.listen(3000, function () {    
     console.log("Server started on port 3000");
-});
+}); 
